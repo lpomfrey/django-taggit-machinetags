95faef72090d02e02d640593a24398a06f743f11
Luke Pomfrey <luke@lukepomfrey.org>
2013-07-10 17:02:05 +0100

    Fix migrations

--- taggit_machinetags/migrations/0001_initial.py
+++ taggit_machinetags/migrations/0001_initial.py
@@ -9,70 +9,58 @@ class Migration(SchemaMigration):
 
     def forwards(self, orm):
         # Adding model 'MachineTag'
-        db.create_table('machinetags_machinetag', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+        db.create_table(u'taggit_machinetags_machinetag', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('name', self.gf('django.db.models.fields.CharField')(max_length=100)),
             ('name_slug', self.gf('django.db.models.fields.SlugField')(max_length=100)),
             ('namespace', self.gf('django.db.models.fields.CharField')(default=u'', max_length=100, blank=True)),
             ('namespace_slug', self.gf('django.db.models.fields.SlugField')(default=u'', max_length=100, blank=True)),
             ('slug', self.gf('django.db.models.fields.SlugField')(unique=True, max_length=201)),
         ))
-        db.send_create_signal('machinetags', ['MachineTag'])
-
-        # Adding unique constraint on 'MachineTag', fields ['namespace', 'name']
-        db.create_unique('machinetags_machinetag', ['namespace', 'name'])
-
-        # Adding unique constraint on 'MachineTag', fields ['namespace_slug', 'name_slug']
-        db.create_unique('machinetags_machinetag', ['namespace_slug', 'name_slug'])
+        db.send_create_signal(u'taggit_machinetags', ['MachineTag'])
 
         # Adding model 'MachineTaggedItem'
-        db.create_table('machinetags_machinetaggeditem', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+        db.create_table(u'taggit_machinetags_machinetaggeditem', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('object_id', self.gf('django.db.models.fields.IntegerField')(db_index=True)),
-            ('content_type', self.gf('django.db.models.fields.related.ForeignKey')(related_name='machinetags_machinetaggeditem_tagged_items', to=orm['contenttypes.ContentType'])),
-            ('tag', self.gf('django.db.models.fields.related.ForeignKey')(related_name=u'machinetags_machinetaggeditem_items', to=orm['machinetags.MachineTag'])),
+            ('content_type', self.gf('django.db.models.fields.related.ForeignKey')(related_name=u'taggit_machinetags_machinetaggeditem_tagged_items', to=orm['contenttypes.ContentType'])),
+            ('tag', self.gf('django.db.models.fields.related.ForeignKey')(related_name=u'taggit_machinetags_machinetaggeditem_items', to=orm['taggit_machinetags.MachineTag'])),
         ))
-        db.send_create_signal('machinetags', ['MachineTaggedItem'])
+        db.send_create_signal(u'taggit_machinetags', ['MachineTaggedItem'])
 
 
     def backwards(self, orm):
-        # Removing unique constraint on 'MachineTag', fields ['namespace_slug', 'name_slug']
-        db.delete_unique('machinetags_machinetag', ['namespace_slug', 'name_slug'])
-
-        # Removing unique constraint on 'MachineTag', fields ['namespace', 'name']
-        db.delete_unique('machinetags_machinetag', ['namespace', 'name'])
-
         # Deleting model 'MachineTag'
-        db.delete_table('machinetags_machinetag')
+        db.delete_table(u'taggit_machinetags_machinetag')
 
         # Deleting model 'MachineTaggedItem'
-        db.delete_table('machinetags_machinetaggeditem')
+        db.delete_table(u'taggit_machinetags_machinetaggeditem')
 
 
     models = {
-        'contenttypes.contenttype': {
+        u'contenttypes.contenttype': {
             'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
             'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
         },
-        'machinetags.machinetag': {
-            'Meta': {'unique_together': "((u'namespace', u'name'), (u'namespace_slug', u'name_slug'))", 'object_name': 'MachineTag'},
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+        u'taggit_machinetags.machinetag': {
+            'Meta': {'object_name': 'MachineTag'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
             'name_slug': ('django.db.models.fields.SlugField', [], {'max_length': '100'}),
             'namespace': ('django.db.models.fields.CharField', [], {'default': "u''", 'max_length': '100', 'blank': 'True'}),
             'namespace_slug': ('django.db.models.fields.SlugField', [], {'default': "u''", 'max_length': '100', 'blank': 'True'}),
             'slug': ('django.db.models.fields.SlugField', [], {'unique': 'True', 'max_length': '201'})
         },
-        'machinetags.machinetaggeditem': {
+        u'taggit_machinetags.machinetaggeditem': {
             'Meta': {'object_name': 'MachineTaggedItem'},
-            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'machinetags_machinetaggeditem_tagged_items'", 'to': "orm['contenttypes.ContentType']"}),
-            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "u'taggit_machinetags_machinetaggeditem_tagged_items'", 'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'object_id': ('django.db.models.fields.IntegerField', [], {'db_index': 'True'}),
-            'tag': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "u'machinetags_machinetaggeditem_items'", 'to': "orm['machinetags.MachineTag']"})
+            'tag': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "u'taggit_machinetags_machinetaggeditem_items'", 'to': u"orm['taggit_machinetags.MachineTag']"})
         }
     }
 
-    complete_apps = ['machinetags']
+    complete_apps = ['taggit_machinetags']
