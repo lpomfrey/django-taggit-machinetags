5517874b9795b7389ac610a9b216768d52a698a3
Luke Pomfrey <luke.pomfrey@titanemail.com>
2014-11-05 09:25:23 +0000

    test runner, tox, and travis updates

--- runtests.py
+++ runtests.py
@@ -4,15 +4,19 @@ from __future__ import unicode_literals
 import os
 import sys
 
+import django
+
 
 os.environ['PYTHONPATH'] = os.path.dirname(__file__)
 os.environ['DJANGO_SETTINGS_MODULE'] = 'test_project.settings'
 
 
 def runtests():
+    if django.VERSION >= (1, 7):
+        django.setup()
     from django.conf import settings
     from django.test.utils import get_runner
-    test_runner = get_runner(settings)()
+    test_runner = get_runner(settings)(verbosity=2)
     failures = test_runner.run_tests(['taggit_machinetags'])
     sys.exit(failures)
 
