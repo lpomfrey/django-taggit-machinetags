09cbeedef3047f1a61a31cfad42d58dc6e25c58e
Paul Monk <git@paulmonk.org>
2017-12-21 10:47:28 +0000

    Fix test project urls import

--- test_project/urls.py
+++ test_project/urls.py
@@ -1,17 +1,26 @@
-from django.conf.urls import patterns, include, url
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+import django
+from pkg_resources import parse_version
+
+if parse_version(django.__version__) >= parse_version('2.0.0'):
+    from django.urls import include, path
+else:
+    from django.conf.urls import include, url
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = [
     # Examples:
-    # url(r'^$', 'test_project.views.home', name='home'),
-    # url(r'^test_project/', include('test_project.foo.urls')),
+    # path('', 'test_project.views.home', name='home'),
+    # path('test_project/', include('test_project.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    # path('admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-)
+    # path('admin/', include(admin.site.urls)),
+]
