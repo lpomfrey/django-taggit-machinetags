51797f747d439bebb8782d1720adf2a777d0a970
Paul Monk <git@paulmonk.org>
2017-12-21 13:34:22 +0000

    Update the test_project urls to use a try/except and revert the example urls back to how it was previously

--- test_project/urls.py
+++ test_project/urls.py
@@ -1,13 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-import django
-from pkg_resources import parse_version
-
-if parse_version(django.__version__) >= parse_version('2.0.0'):
-    from django.urls import include, path
-else:
+try:
     from django.conf.urls import include, url
+except ImportError:
+    from django.urls import include, url
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -15,12 +12,12 @@ else:
 
 urlpatterns = [
     # Examples:
-    # path('', 'test_project.views.home', name='home'),
-    # path('test_project/', include('test_project.foo.urls')),
+    # url(r'^$', 'test_project.views.home', name='home'),
+    # url(r'^test_project/', include('test_project.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # path('admin/doc/', include('django.contrib.admindocs.urls')),
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # path('admin/', include(admin.site.urls)),
+    # url(r'^admin/', include(admin.site.urls)),
 ]
