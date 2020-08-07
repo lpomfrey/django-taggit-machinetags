c9f4de96d0d4874264935c2d1282cf3b7213662f
Luke Pomfrey <luke@lukepomfrey.org>
2020-08-07 09:56:35 +0100

    Move metadata etc to setup.cfg

--- setup.py
+++ setup.py
@@ -1,27 +1,8 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import os
-import re
 import sys
-from setuptools import setup, find_packages
-
-
-def get_version(package):
-    '''
-    Return package version as listed in `__version__` in `init.py`.
-    '''
-    init_py = open(os.path.join(package, '__init__.py')).read()
-    return re.search(
-        '^__version__ = [\'"]([^\'"]+)[\'"]', init_py, re.MULTILINE
-    ).group(1)
-
-
-version = get_version('taggit_machinetags')
-
-required = [
-    'django-taggit>=0.12'
-]
+from setuptools import setup
 
 
 _PUBLISH_WARNING = '''
@@ -49,36 +30,4 @@ if sys.argv[-1] == 'publish':
     sys.exit()
 
 
-setup(
-    name='django-taggit-machinetags',
-    version=version,
-    url='http://github.com/lpomfrey/django-taggit-machinetags',
-    license='BSD',
-    description='Machine tagging built upon django-taggit',
-    author='Luke Pomfrey',
-    author_email='lpomfrey@gmail.com',
-    packages=find_packages(exclude=['test_project']),
-    install_requires=required,
-    tests_require=['mock'],
-    test_suite='runtests.runtests',
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Framework :: Django',
-        'Framework :: Django :: 1.8',
-        'Framework :: Django :: 1.11',
-        'Framework :: Django :: 2.0',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-    ]
-)
+setup(test_suite='runtests.runtests')
